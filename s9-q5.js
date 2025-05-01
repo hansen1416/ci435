88186505229ae4fb89f18cef0701df2a6d51959f
Longzhen Han <badapplesweetie@gmail.com>
2025-05-01 10:17:59 +0100

    Create s9-q5.js
    1

---
+++ s9-q5.js
@@ -0,0 +1,28 @@
+/**
+Write a function called rotate that takes two arguments (arr, an array, and num, a number),
+which will modify the arr array by popping an element off the last element and splicing it at the
+start of the array. This should be repeated num times, effectively rotating the array. For example:
+rotate(["Harry","Sarah","Oscar","Tina"], 2) ->
+["Oscar","Tina","Harry","Sarah"];
+ */
+
+function rotate(arr, num) {
+    for (let i = 0; i < num; i++) {
+
+        popped = arr.pop();
+
+        console.log(popped)
+
+        // prepend the popped item (last item) to the beginning of the array
+        arr = [popped].concat(arr);
+    }
+
+    return arr;
+}
+
+const a1 = ["Harry","Sarah","Oscar","Tina"];
+const gfdgdg = 2;
+
+const res = rotate(a1, gfdgdg);
+
+console.log(res);
