7fc2e10cd53875074d74c1cc8df2119695f83c44
hansen1416 <badapplesweetie@gmail.com>
2025-05-02 09:30:48 +0100

    '1';

--- js/week9/tutorial6-q5.js
+++ js/week9/tutorial6-q5.js
@@ -1,11 +1,3 @@
-/**
-Write a function called rotate that takes two arguments (arr, an array, and num, a number),
-which will modify the arr array by popping an element off the last element and splicing it at the
-start of the array. This should be repeated num times, effectively rotating the array. For example:
-rotate(["Harry","Sarah","Oscar","Tina"], 2) ->
-["Oscar","Tina","Harry","Sarah"];
- */
-
 function rotate(arr, num) {
     for (let i = 0; i < num; i++) {
 
